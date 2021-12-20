630a27d3a93522c887c537a0172fb44e81e67c1b
Alex Curran <alexcurran.va@gmail.com>
2021-12-20 15:10:42 -0600

    Working recursive solution for grid-traveler.js

---
+++ grid-traveler.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * How many ways can you travel on a grid of m rows by n columns?
+ * Movement is restricted to only right or down movements on the grid.
+ * 
+ * Time: O(n^2)
+ * Space: O(n)
+ * 
+ * @param {number} m 
+ * @param {number} n 
+ * @returns {number}
+ */
+function gridTraveler(m, n) {
+    // base case
+    if (m === 1 && n === 1) return 1;
+    // base case
+    if (m === 0 || n === 0) return 0;
+
+    return gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
+}
+
+console.log( gridTraveler(1, 1) ); // 1
+console.log( gridTraveler(2, 3) ); // 3
+console.log( gridTraveler(3, 2) ); // 3
+console.log( gridTraveler(3, 3) ); // 6
