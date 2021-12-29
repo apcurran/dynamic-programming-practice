4e1d525cfecbb72ccf982b676ff60a4d57ea90e8
Alex Curran <alexcurran.va@gmail.com>
2021-12-29 17:10:38 -0600

    Add memoized version

--- sum-possible.js
+++ sum-possible.js
@@ -1,24 +1,58 @@
 "use strict";
 
+// /**
+//  * The function sumPossible takes in an amount and an array of positive numbers.
+//  * The function should return a boolean indicating whether or not it is possible to create the amount by summing numbers of the array.
+//  * You may reuse numbers of the array as many times as necessary.
+//  * You may assume that the target amount is non-negative.
+//  * 
+//  * Solution 1 -- brute force
+//  * 
+//  * m = target sum
+//  * n = array length
+//  * 
+//  * Time: O(n^m)
+//  * Space: O(m)
+//  * 
+//  * @param {number} amount
+//  * @param {number[]} numbers
+//  * @returns {boolean}
+//  */
+// function sumPossible(amount, numbers) {
+//     if (amount === 0) { // base case 1
+//         return true;
+//     } else if (amount < 0) { // base case 2
+//         return false;
+//     }
+
+//     for (let num of numbers) {
+//         const remainder = amount - num;
+
+//         if (sumPossible(remainder, numbers) === true) {
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
+
 /**
- * The function sumPossible takes in an amount and an array of positive numbers.
- * The function should return a boolean indicating whether or not it is possible to create the amount by summing numbers of the array.
- * You may reuse numbers of the array as many times as necessary.
- * You may assume that the target amount is non-negative.
- * 
- * Solution 1 -- brute force
+ * Solution 2 -- memo version
  * 
  * m = target sum
  * n = array length
  * 
- * Time: O(n^m)
+ * Time: O()
  * Space: O(m)
  * 
  * @param {number} amount
  * @param {number[]} numbers
  * @returns {boolean}
  */
-function sumPossible(amount, numbers) {
+function sumPossible(amount, numbers, memo = new Map()) {
+    // Base case for memo retrieval
+    if (memo.has(amount)) return memo.get(amount);
+
     if (amount === 0) { // base case 1
         return true;
     } else if (amount < 0) { // base case 2
@@ -28,13 +62,19 @@ function sumPossible(amount, numbers) {
     for (let num of numbers) {
         const remainder = amount - num;
 
-        if (sumPossible(remainder, numbers) === true) {
+        if (sumPossible(remainder, numbers, memo) === true) {
+            // cache in memo
+            memo.set(amount, true);
+
             return true;
         }
     }
+    // cache in memo
+    memo.set(amount, false);
 
     return false;
 }
 
 console.log( sumPossible(8, [5, 12, 4]) ); // -> true, 4 + 4;
 console.log( sumPossible(15, [6, 2, 10, 19]) ); // -> false
+console.log( sumPossible(300, [7, 14]) ); // -> false (slow without memoization)
