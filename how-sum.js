769fc442b7a19afe11b842fee6fc3cf8dc0dfa6e
Alex Curran <alexcurran.va@gmail.com>
2021-12-30 12:25:35 -0600

    Brute force solution for how-sum.js

--- how-sum.js
+++ how-sum.js
@@ -5,12 +5,35 @@
  * If there is no combo that adds up to the targetSum, then return null.
  * You may return any combo if multiples are possible.
  * 
+ * Solution 1 -- brute force
+ * 
+ * m = targetSum
+ * n = numbersArr.length
+ * 
+ * Time: O(n^m * m)
+ * Space: O(m)
+ * 
  * @param {number} targetSum 
  * @param {number[]} numbersArr 
  * @returns {number[]|null}
  */
 function howSum(targetSum, numbersArr) {
-    
+    if (targetSum === 0) {
+        return [];
+    } else if (targetSum < 0) {
+        return null;
+    }
+
+    for (let num of numbersArr) {
+        const remainder = targetSum - num;
+        const remainderResult = howSum(remainder, numbersArr);
+
+        if (remainderResult !== null) {
+            return [...remainderResult, num];
+        }
+    }
+
+    return null;
 }
 
 console.log( howSum(7, [5, 3, 4, 7]) ); // [3, 4] or [7]
