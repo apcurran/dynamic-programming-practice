58405ce0e01245d37f64a41f3f0b6145658ea718
Alex Curran <alexcurran.va@gmail.com>
2022-01-04 11:58:12 -0600

    Add memo solution

--- can-construct.js
+++ can-construct.js
@@ -1,20 +1,52 @@
 "use strict";
 
 
+// /**
+//  * Solution 1 -- brute force
+//  * 
+//  * m = target.length
+//  * n = wordBank.length
+//  * 
+//  * Time: O((n^m) * m)
+//  * Space: O(m^2)
+//  * 
+//  * @param {string} target 
+//  * @param {string[]} wordBank 
+//  * @returns {boolean}
+//  */
+// function canConstruct(target, wordBank) {
+//     if (target === "") return true;
+
+//     for (let word of wordBank) {
+//         if (target.startsWith(word)) {
+//             const endOfWord = word.length;
+//             const suffix = target.slice(endOfWord);
+
+//             if (canConstruct(suffix, wordBank) === true) {
+//                 return true;
+//             }
+//         }
+//     }
+
+//     return false;
+// }
+
 /**
- * Solution 1 -- brute force
+ * Solution 2 -- memo ver
  * 
  * m = target.length
  * n = wordBank.length
  * 
- * Time: O((n^m) * m)
+ * Time: O(n * m^2)
  * Space: O(m^2)
  * 
  * @param {string} target 
  * @param {string[]} wordBank 
  * @returns {boolean}
  */
-function canConstruct(target, wordBank) {
+function canConstruct(target, wordBank, memo = new Map()) {
+    if (memo.has(target)) return memo.get(target);
+
     if (target === "") return true;
 
     for (let word of wordBank) {
@@ -22,14 +54,22 @@ function canConstruct(target, wordBank) {
             const endOfWord = word.length;
             const suffix = target.slice(endOfWord);
 
-            if (canConstruct(suffix, wordBank) === true) {
+            if (canConstruct(suffix, wordBank, memo) === true) {
+                memo.set(target, true);
+
                 return true;
             }
         }
     }
+
+    memo.set(target, false);
 
     return false;
 }
 
 console.log( canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]) ); // true
 console.log( canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]) ); // false
+console.log(
+    canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+                ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"])
+); // false
