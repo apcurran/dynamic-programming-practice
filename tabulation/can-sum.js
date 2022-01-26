969f4d2c743997340f1aec1472348a8aa86f97e0
Alex Curran <alexcurran.va@gmail.com>
2022-01-26 10:57:37 -0600

    Working solution for can-sum.js

--- tabulation/can-sum.js
+++ tabulation/can-sum.js
@@ -1,10 +1,30 @@
 /**
  * Solution
+ * m = targetSum
+ * n = numsArr.length
+ * time: O(m * n)
+ * space: O(m)
  * 
- * @param {number} target 
+ * @param {number} targetSum
  * @param {number[]} numsArr 
  * @returns {boolean[]}
  */
-function canSum(target, numsArr) {
+function canSum(targetSum, numsArr) {
+    let table = Array(targetSum + 1).fill(false);
+    // targetSum of 0 is always possible to be made by taking no vals from numsArr
+    table[0] = true;
 
+    for (let i = 0; i <= targetSum; i++) {
+        if (table[i] === true) {
+            for (let num of numsArr) {
+                table[i + num] = true;
+            }
+        }
+    }
+
+    return table[targetSum];
 }
+
+console.log( canSum(7, [2, 3]) ); // true
+console.log( canSum(7, [5, 3, 4, 7]) ); // true
+console.log( canSum(7, [2, 4]) ); // false
