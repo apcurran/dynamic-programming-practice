36404eca2221229c3c2d236cd0d66cc8f9db035a
Alex Curran <alexcurran.va@gmail.com>
2023-11-30 10:54:23 -0600

    Memoized solution

--- recursion/min-change.js
+++ recursion/min-change.js
@@ -1,27 +1,72 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * a = amount
+//  * c = coins array length
+//  * time: O(c^a)
+//  * space: O(a)
+//  * 
+//  * @param {number} amount 
+//  * @param {number[]} coins 
+//  * @returns {number}
+//  */
+// function minChange(amount, coins) {
+//     if (amount === 0) return 0;
+
+//     // not possible to make the subamount
+//     if (amount < 0) return -1;
+
+//     let minCoins = -1;
+
+//     for (let coin of coins) {
+//         const subAmount = amount - coin;
+//         const subCoinsCount = minChange(subAmount, coins);
+
+//         if (subCoinsCount !== -1) {
+//             // it is possible to make subamount
+//             const numOfCoins = subCoinsCount + 1;
+
+//             // if numOfCoins is smaller than current minCoins OR
+//             // minCoins still has default value of -1
+//             // update minCoins
+//             if (numOfCoins < minCoins || minCoins === -1) {
+//                 minCoins = numOfCoins;
+//             }
+//         }
+//     }
+
+//     return minCoins;
+// }
+
 /**
- * solution 1
+ * solution 2 -- hashmap cache
  * a = amount
  * c = coins array length
- * time: O(c^a)
+ * time: O(a * c)
  * space: O(a)
  * 
  * @param {number} amount 
  * @param {number[]} coins 
+ * @param {Map=} cache
  * @returns {number}
  */
-function minChange(amount, coins) {
+function minChange(amount, coins, cache = new Map()) {
     if (amount === 0) return 0;
 
     // not possible to make the subamount
     if (amount < 0) return -1;
 
+    // check if value is in cache
+    if (cache.has(amount)) {
+        return cache.get(amount);
+    }
+
     let minCoins = -1;
 
     for (let coin of coins) {
         const subAmount = amount - coin;
-        const subCoinsCount = minChange(subAmount, coins);
+        const subCoinsCount = minChange(subAmount, coins, cache);
 
         if (subCoinsCount !== -1) {
             // it is possible to make subamount
@@ -35,6 +80,9 @@ function minChange(amount, coins) {
             }
         }
     }
+
+    // add value to cache
+    cache.set(amount, minCoins);
 
     return minCoins;
 }
