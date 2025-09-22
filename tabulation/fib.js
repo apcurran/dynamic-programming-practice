fa67498c7fd6268134aab2120709024d1104064e
Alex Curran <alexcurran.va@gmail.com>
2025-09-22 16:44:02 -0500

    Working for large numbers bottoms-up approach

--- tabulation/fib.js
+++ tabulation/fib.js
@@ -1,16 +1,35 @@
 "use strict";
 
+// /**
+//  * Solution 1
+//  * time: O(n)
+//  * space: O(n)
+//  *
+//  * @param {number} n
+//  * @returns {number}
+//  */
+// function fib(n) {
+//     let table = Array(n + 1).fill(0);
+//     table[1] = 1;
+
+//     for (let i = 2; i <= n; i++) {
+//         table[i] = table[i - 1] + table[i - 2];
+//     }
+
+//     return table[n];
+// }
+
 /**
- * Solution 1
+ * Solution 2 -- allow for large fib values
  * time: O(n)
  * space: O(n)
  *
  * @param {number} n
- * @returns {number}
+ * @returns {bigint}
  */
 function fib(n) {
-    let table = Array(n + 1).fill(0);
-    table[1] = 1;
+    let table = Array(n + 1).fill(0n);
+    table[1] = 1n;
 
     for (let i = 2; i <= n; i++) {
         table[i] = table[i - 1] + table[i - 2];
@@ -19,7 +38,8 @@ function fib(n) {
     return table[n];
 }
 
-console.log(fib(6)); // 8
-console.log(fib(7)); // 13
-console.log(fib(8)); // 21
-console.log(fib(50)); // 12586269025
+console.log(fib(6)); // 8n
+console.log(fib(7)); // 13n
+console.log(fib(8)); // 21n
+console.log(fib(50)); // 12586269025n
+console.log(fib(100)); // 354224848179261915075n
